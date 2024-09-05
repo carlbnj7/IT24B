889a070ad163fa871abe6ec746d08894d215baf5
carlbnj7 <carlbenedictnj7@gmail.com>
2024-09-05 13:00:49 +0800

    added app.js

---
+++ app.js
@@ -0,0 +1,61 @@
+// Data types
+//String
+
+let name = "Carlb";
+console.log("Your name: " + name);
+let age = 19;
+console.log ("Your age: " + age);
+
+ 
+
+//Arithmetic and Logical Operators
+
+let a = 100;
+let b = 20;
+let diff = a - b;
+console.log("My grades: " + diff);
+
+
+//Conditional Statement
+
+if(age>=50){
+    console.log("You passed");
+}else;
+    console.log("You failed");
+
+
+    let grado = 80;
+    let category;
+
+    switch(true){
+        case (grado>=75):
+            category = "You passed";
+            break;
+            default:
+                category = "You failed";
+                break;
+
+                console.log("You still survived");
+
+                //loops
+
+                const activities = ["pray","eat","sleep"];
+                for (let w = 0; w < activities.length; w++){
+                    console.assert;ongamepadconnected('activities ${w + 1}: ${activities[w]}');
+
+                }
+}
+// Functional Programming
+// Determine odd or even
+
+function CheckNumber(Number){
+    const numberToCheck = 30;
+    if (number % 2 === 0){
+        console.log('${number} is even.');
+    }else if (isPrime(number)) {
+        console.log('${number} is prime.');
+    }else{
+        console.log('${number} is niether even nor prime.');
+    }
+    }
+    console.log("The number is prime");
